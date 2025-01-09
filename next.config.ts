e7273ae8cc93fb27f4eac55aae38e294599b17d0
Raiza <wisdomadebola62@gmail.com>
2025-01-09 20:26:35 +0100

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oceanic-okapi-791.convex.cloud"
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com"
+      },
+    ]
+  }
 };
 
 export default nextConfig;
